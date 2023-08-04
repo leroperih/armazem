23054b134665b3ac78c902409f47fb757add6bfc
Lero Perih <1joao2victor3santana4ferreira@gmail.com>
2023-08-04 04:16:48 -0300

    mudei mas não sei o que, talvez o menu.

--- armazem/conteudo/script/script.js
+++ armazem/conteudo/script/script.js
@@ -1,19 +1,12 @@
-
+div_dis_op = document.querySelector("div#header-container-links")
 
 function clickMen() {
-nalin = document.querySelector("nav#links-socials")
-contso = document.querySelector("div#container-nav")
 
-
-    if ( nalin.style.display == 'none' ) {
-    nalin = document.querySelector("nav#links-socials")
-    contso = document.querySelector("div#container-nav")
-    nalin.style.display = 'block'
-    contso.style.backgroundColor = '#050505'
-} else {
-    nalin = document.querySelector("nav#links-socials")
-    contso = document.querySelector("div#container-nav")
-    nalin.style.display = 'none'
-    contso.style.backgroundColor = '#05050500'
-}
+  if ( div_dis_op.style.display == 'none' ) {
+    div_dis_op = document.querySelector("div#header-container-links")
+    div_dis_op.style.display = 'block'
+  } else {
+    div_dis_op = document.querySelector("div#header-container-links")
+    div_dis_op.style.display = 'none'
+  }
 }
